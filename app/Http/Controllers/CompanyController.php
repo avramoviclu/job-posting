30fedfb26cf9b7a690f09db5d909ca625e19a844
Luka Avramovic <avramoviclu@gmail.com>
2023-04-23 18:40:42 +0200

    Updates CompanyController.

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -6,77 +6,38 @@ use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    /**
-        * Display a listing of the resource.
-        *
-        * @return Response
-        */
-        public function index()
-        {
-            //
-        }
-    
-        /**
-            * Show the form for creating a new resource.
-            *
-            * @return Response
-            */
-        public function create()
-        {
-            //
-        }
-    
-        /**
-            * Store a newly created resource in storage.
-            *
-            * @return Response
-            */
-        public function store()
-        {
-            //
-        }
-    
-        /**
-            * Display the specified resource.
-            *
-            * @param  string  $ulid
-            * @return Response
-            */
-        public function show($ulid)
-        {
-            //
-        }
-    
-        /**
-            * Show the form for editing the specified resource.
-            *
-            * @param  string  $ulid
-            * @return Response
-            */
-        public function edit($ulid)
-        {
-            //
-        }
-    
-        /**
-            * Update the specified resource in storage.
-            *
-            * @param  int  $id
-            * @return Response
-            */
-        public function update($id)
-        {
-            //
-        }
-    
-        /**
-            * Remove the specified resource from storage.
-            *
-            * @param  string  $ulid
-            * @return Response
-            */
-        public function destroy($ulid)
-        {
-            //
-        }
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($ulid)
+    {
+        //
+    }
+
+    public function edit($ulid)
+    {
+        //
+    }
+
+    public function update($id)
+    {
+        //
+    }
+
+    public function destroy($ulid)
+    {
+        //
+    }
 }
