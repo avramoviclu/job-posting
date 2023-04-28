50c598fae500329b925b4b634ae8dc53538f20da
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 19:58:29 +0200

    Updates CompanyService.

--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -13,37 +13,17 @@ class CompanyService {
         $this->companyRepository = $companyRepository;
     }
 
-    public function index()
-    {
-        //
-    }
-
     public function create()
     {
         //
     }
 
-    public function store()
+    public function update()
     {
         //
     }
 
-    public function show($ulid)
-    {
-        //
-    }
-
-    public function edit($ulid)
-    {
-        //
-    }
-
-    public function update($id)
-    {
-        //
-    }
-
-    public function destroy($ulid)
+    public function delete()
     {
         //
     }
