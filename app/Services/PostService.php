193ea5c580218ccd151f19215ca5d247556c9968
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 19:59:52 +0200

    Updates PostService.

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -8,42 +8,17 @@ class PostService {
 
     protected PostRepository $postRepository;
 
-    public function __construct(PostRepository $postRepository)
-    {
-        $this->postRepository = $postRepository;
-    }
-
-    public function index()
-    {
-        //
-    }
-
     public function create()
     {
         //
     }
 
-    public function store()
+    public function update()
     {
         //
     }
 
-    public function show($ulid)
-    {
-        //
-    }
-
-    public function edit($ulid)
-    {
-        //
-    }
-
-    public function update($id)
-    {
-        //
-    }
-
-    public function destroy($ulid)
+    public function delete()
     {
         //
     }
