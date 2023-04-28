267e24cf7642a55d48f4d17d928c22f422eec56e
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 19:46:26 +0200

    Updates CompanyService.

--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Repositories;
+
 use App\Interfaces\CompanyInterface;
 
 class CompanyRepository implements CompanyInterface
--- app/Services/CompanyService.php
+++ app/Services/CompanyService.php
@@ -1,7 +1,50 @@
 <?php
 
 namespace App\Services;
+
+use App\Repositories\CompanyRepository;
  
 class CompanyService {
-    //
+
+    protected CompanyRepository $companyRepository;
+
+    public function __construct(CompanyRepository $companyRepository)
+    {
+        $this->companyRepository = $companyRepository;
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($ulid)
+    {
+        //
+    }
+
+    public function edit($ulid)
+    {
+        //
+    }
+
+    public function update($id)
+    {
+        //
+    }
+
+    public function destroy($ulid)
+    {
+        //
+    }
 }
