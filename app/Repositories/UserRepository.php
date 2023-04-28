234dfb4eaad203667decef4fea5f81e10bdcd386
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 19:49:53 +0200

    Updates UserService.

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Repositories;
+
 use App\Interfaces\UserInterface;
 
 class UserRepository implements UserInterface
--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,7 +1,50 @@
 <?php
 
 namespace App\Services;
+
+use App\Repositories\UserRepository;
  
 class UserService {
-    //
+
+    protected UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($ulid)
+    {
+        //
+    }
+
+    public function edit($ulid)
+    {
+        //
+    }
+
+    public function update($id)
+    {
+        //
+    }
+
+    public function destroy($ulid)
+    {
+        //
+    }
 }
