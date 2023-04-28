085f17ff946a8176cdb3041e6aa2c198ef31f3e2
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 20:03:37 +0200

    Updates Post interface and repository.

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -6,5 +6,15 @@ use App\Interfaces\PostInterface;
 
 class PostRepository implements PostInterface
 {
+    public function create() {
 
+    }
+
+    public function update(string $id) {
+
+    }
+
+    public function delete(string $id) {
+        
+    }
 }
