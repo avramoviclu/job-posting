6e22e5a5c6b52b14e05e1f05de30f42d45fe0e18
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 19:47:44 +0200

    Updates PostService.

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Repositories;
+
 use App\Interfaces\PostInterface;
 
 class PostRepository implements PostInterface
--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -1,7 +1,50 @@
 <?php
 
 namespace App\Services;
+
+use App\Repositories\PostRepository;
  
 class PostService {
-    //
+
+    protected PostRepository $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store()
+    {
+        //
+    }
+
+    public function show($ulid)
+    {
+        //
+    }
+
+    public function edit($ulid)
+    {
+        //
+    }
+
+    public function update($id)
+    {
+        //
+    }
+
+    public function destroy($ulid)
+    {
+        //
+    }
 }
