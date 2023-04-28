8846d1ea4d8ad9e20ce97ef9c3bed6c5ce9359e5
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 20:00:13 +0200

    Updates UserService.php

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -13,37 +13,17 @@ class UserService {
         $this->userRepository = $userRepository;
     }
 
-    public function index()
-    {
-        //
-    }
-
     public function create()
     {
         //
     }
 
-    public function store()
+    public function update()
     {
         //
     }
 
-    public function show($ulid)
-    {
-        //
-    }
-
-    public function edit($ulid)
-    {
-        //
-    }
-
-    public function update($id)
-    {
-        //
-    }
-
-    public function destroy($ulid)
+    public function delete()
     {
         //
     }
