0ea2ea6a17860b6fc529dbb652ca4b500b15295a
Luka Avramovic <avramoviclu@gmail.com>
2023-04-28 20:02:55 +0200

    Updates Company interface and repository.

--- app/Interfaces/CompanyInterface.php
+++ app/Interfaces/CompanyInterface.php
@@ -1,8 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Interfaces;
 
 interface CompanyInterface
 {
+    public function create();
 
+    public function update(string $id);
+
+    public function delete(string $id);
 }
--- app/Repositories/CompanyRepository.php
+++ app/Repositories/CompanyRepository.php
@@ -6,5 +6,15 @@ use App\Interfaces\CompanyInterface;
 
 class CompanyRepository implements CompanyInterface
 {
+    public function create() {
 
+    }
+
+    public function update(string $id) {
+
+    }
+
+    public function delete(string $id) {
+        
+    }
 }
