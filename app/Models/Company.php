613aaf0444936e93436a36bb870db28d70bc9def
Luka Avramovic <avramoviclu@gmail.com>
2023-04-29 20:35:46 +0200

    Creates Company model.

---
+++ app/Models/Company.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Company extends Model
+{
+    use HasFactory, HasUlids, SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'timezone',
+        'email'
+    ];
+}
