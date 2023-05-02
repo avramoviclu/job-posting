39321596c7724e231a511a44ea988d4857f257cd
Luka Avramovic <avramoviclu@gmail.com>
2023-05-02 18:33:12 +0200

    Updates custom requests.

--- app/Http/Requests/Post/DeletePostRequest.php
+++ app/Http/Requests/Post/DeletePostRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Post;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class CreateUserRequest extends FormRequest
+class DeletePostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
